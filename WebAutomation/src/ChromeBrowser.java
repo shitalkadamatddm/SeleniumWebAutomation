206e99f268ad4c3e05bee1add9cc99a111cef220
venka <venka@DESKTOP-AJRFL20>
2019-11-27 18:05:36 +0000

    driver methods

--- WebAutomation/src/ChromeBrowser.java
+++ WebAutomation/src/ChromeBrowser.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,7 +13,11 @@ public class ChromeBrowser
 	System.out.println(driver.getCurrentUrl());
 	//method to get pagesource of webpage
 	//System.out.println(driver.getPageSource());
-	
+	driver.navigate().to("https://www.skyscanner.net/");
+	//driver.navigate().back();
+	//driver.navigate().forward();
+	driver.findElement(By.className("login")).click();
+	//driver.close();
 Testcase1 t = new Testcase1();
 t.ExecutePrint();
 System.out.println(t.ReturingString());
