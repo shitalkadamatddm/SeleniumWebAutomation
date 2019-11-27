88f3b0309fcf8860ec864e58fb627f9895d6d6fd
venka <venka@DESKTOP-AJRFL20>
2019-11-27 11:17:13 +0000

    First commit, created 1 class which chromeBrowser

---
+++ WebAutomation/src/ChromeBrowser.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowser 
+{
+	public static void main(String[] args) 
+{
+	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com/");
+	System.out.println(driver.getTitle());
+}
+}
